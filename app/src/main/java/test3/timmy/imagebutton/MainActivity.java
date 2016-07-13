e581b93f149fa19cc1cae092300a81af010157c6
timmy1234 <timmy@emt.com.tw>
2016-07-13 09:13:41 +0800

    init

---
+++ app/src/main/java/test3/timmy/imagebutton/MainActivity.java
@@ -0,0 +1,31 @@
+package test3.timmy.imagebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextPaint;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton cmorechat;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//
+//        cmorechat = (ImageButton)findViewById(R.id.imageButton);
+//
+//        cmorechat.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //TODO
+//            }
+//        });
+//
+//        TextPaint t = new TextPaint();
+//        t.getFontMetrics().
+
+    }
+}
